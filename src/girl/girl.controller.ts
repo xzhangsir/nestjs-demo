91ebbbbd077bf7dbcb9ca24457d3e29a5b5c66f7
zhangxin <2916733574@qq.com>
2022-12-08 23:26:31 +0800

    get和post  RESTClinet

--- src/girl/girl.controller.ts
+++ src/girl/girl.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, Query, Body } from '@nestjs/common';
 import { GirlService } from './girl.service';
 
 @Controller('girl')
@@ -7,5 +7,31 @@ export class GirlController {
   @Get()
   getGirls(): any {
     return this.girlService.getGirls();
+  }
+  /*  @Post('add') //  @Post('/add')
+  addGril(): any {
+    return this.girlService.addGirl();
+  } */
+  /* @Post('add') 
+  addGril(@Request() req): any {
+    console.log(req.body);
+    return this.girlService.addGirl();
+  } */
+  @Post('add')
+  addGril(@Body() body): any {
+    console.log(body);
+    return this.girlService.addGirl(body);
+  }
+  /*  @Get('getGirlById')
+  getGirlById(@Request() req): any {
+    // console.log(req);
+    const id: number = parseInt(req.query.id);
+    return this.girlService.getGirlById(id);
+  } */
+  @Get('getGirlById')
+  getGirlById(@Query() query): any {
+    // console.log(query);
+    const id: number = parseInt(query.id);
+    return this.girlService.getGirlById(id);
   }
 }
--- src/girl/girl.service.ts
+++ src/girl/girl.service.ts
@@ -9,4 +9,37 @@ export class GirlService {
       msg: 'success',
     };
   }
+  /*   addGirl() {
+    return {
+      code: 0,
+      data: { id: 3, name: '小明', age: '12' },
+      msg: 'success',
+    };
+  } */
+  addGirl(body: any) {
+    return {
+      code: 0,
+      data: body,
+      msg: 'success',
+    };
+  }
+  getGirlById(id: number) {
+    let res: any = {};
+    switch (id) {
+      case 1:
+        res = { id: 1, name: '小花' };
+        break;
+      case 2:
+        res = { id: 2, name: '小树' };
+        break;
+      default:
+        res = { id: id, name: 'name' + id };
+        break;
+    }
+    return {
+      code: 0,
+      data: res,
+      msg: 'success',
+    };
+  }
 }
